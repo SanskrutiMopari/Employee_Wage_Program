2dea178110337a0b026c66c0f41c644b54729ae1
Sanskruti Mopari <sanskrutimopari2016@gmail.com>
2022-12-28 22:16:10 +0530

    Added: Daily Employee Wage

---
+++ DailyWage.java
@@ -0,0 +1,26 @@
+package EmployeeWage;
+
+//Calculate Daily Employee Wage
+//Assume Wage per Hour is 20
+//Assume Full Day Hour is 8
+
+public class DailyWage {
+    public static final int IS_FULL_TIME = 1;
+    public static final int EMPLOYEE_WAGE_PER_HOUR = 20;
+
+    public static void main(String[] args) {
+        int empHRS = 8;
+        int empWAGE = 0;
+
+
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else {
+            System.out.println("Employee is Absent");
+            empHRS = 0;
+        }
+        empWAGE = empHRS * EMPLOYEE_WAGE_PER_HOUR;
+        System.out.println("Employee wage is " + empWAGE);
+    }
+}
